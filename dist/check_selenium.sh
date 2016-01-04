0e77b8e66294fd09e6d08740742b263987bbab02
Gert Vandelaer <gert.vandelaer@gmail.com>
2016-01-04 10:48:00 +0100

    update .jars in run_scripts

--- dist/check_selenium.sh
+++ dist/check_selenium.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+WD="$(dirname $0)"
+CPDIR="${WD}/lib"
 
-JAVA_HOME=/opt/java/java5
-DIR=$(/usr/bin/dirname ${0})
-CLASSPATH=${DIR}/lib/commons-logging-1.1.1.jar:${DIR}/lib/httpclient-4.1.2.jar:${DIR}/lib/httpcore-4.1.3.jar:${DIR}/lib/commons-exec-1.1.jar:${DIR}/lib/json-20080701.jar:${DIR}/lib/guava-11.0.1.jar:${DIR}/lib/selenium-java-2.20.0.jar:${DIR}/lib/commons-cli-1.2.jar:${DIR}/lib/junit-4.8.1.jar:${DIR}/com/example/tests/.:${DIR}/lib/check-selenium.jar:${DIR}/lib/selenium-example-tests.jar
+[ -z $JAVA_HOME ] && JAVA_HOME=/eso/jdk1.7.0_15
+CLASSPATH=${CPDIR}/java-getopt-1.0.14.jar:${CPDIR}/junit-4.10.jar:${CPDIR}/check-selenium.jar:${CPDIR}/commons-cli-1.2.jar:${CPDIR}/selenium-server-standalone-2.35.0.jar:${CPDIR}/selenium-tests.jar:${WD}/.
 
-echo $CLASSPATH
-
-java -cp ${CLASSPATH} info.devopsabyss.CallSeleniumTest $@
+$JAVA_HOME/bin/java -cp "${CLASSPATH}" org.godsells.CallSeleniumTest $@
